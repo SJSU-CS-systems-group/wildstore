c14aa62c1a88122cc493807e60a8764649d25bf7
Apoorva Mohite <apps.mohite@gmail.com>
2023-06-08 19:02:00 -0700

    Fixed WildfireFilesCrawler.java

--- wildfirestorage-core/src/main/java/com/sjsu/wildfirestorage/WildfireFilesCrawler.java
+++ wildfirestorage-core/src/main/java/com/sjsu/wildfirestorage/WildfireFilesCrawler.java
@@ -4,14 +4,14 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "GET", mixinStandardHelpOptions = true)
 public class WildfireFilesCrawler implements Runnable {
-    @CommandLine.Parameters(paramLabel = "<option>", defaultValue = "all", description = "Which information to print.")
+    @CommandLine.Parameters(paramLabel = "<option>", defaultValue = "all", description = "Which information to print - 'all' or 'basic'")
     private String option;
     @CommandLine.Parameters(paramLabel = "<file>", description = "File to process")
     private String ncFilePath = "/Users/ysabella/Downloads/wrfout_d01_2019-06-20_18_00_00";
     // private String ncFilePath = "/Users/spartan/Applications/WildStorage/datafiles/met_em.d01.2019-06-21_09:00:00.nc";
     // private String ncFilePath = "/Users/spartan/Applications/WildStorage/datafiles/wrfout_d05_2012-09-11_00:00:0069:00:00.nc";
     @CommandLine.Option(names = "--hostname", description = "Host name of the API server")
-    String hostnaname;
+    String hostname;
 
     public void run() {
         NetcdfFileReader fileReader = new NetcdfFileReader(ncFilePath);
@@ -26,7 +26,7 @@ public class WildfireFilesCrawler implements Runnable {
         if (hostname == null) {
             System.out.println("No hostname specified. Skipping metadata update.");
         } else {
-            Client.post(crawlerArgs.hostname + "/api/metadata", metadata);
+            Client.post(hostname + "/api/metadata", metadata);
         }
     }
     public static void main(String[] args) {
