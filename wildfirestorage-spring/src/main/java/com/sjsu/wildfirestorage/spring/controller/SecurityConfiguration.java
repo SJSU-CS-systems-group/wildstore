d4e848e59e7aa91c7fbde84426ad714e1cbbeda2
Ysabella <ysabelladelacruz2022@gmail.com>
2023-07-13 17:40:06 -0700

    oauth testing

--- wildfirestorage-spring/src/main/java/com/sjsu/wildfirestorage/spring/controller/SecurityConfiguration.java
+++ wildfirestorage-spring/src/main/java/com/sjsu/wildfirestorage/spring/controller/SecurityConfiguration.java
@@ -1,27 +1,85 @@
 package com.sjsu.wildfirestorage.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private CustomOAuth2UserService userService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        /**http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                );**/
+        http
+                .authorizeHttpRequests()
+                    .requestMatchers("/api").permitAll() //api path
+                    .anyRequest().authenticated()
+                    .and()
+                .oauth2Login().defaultSuccessUrl("/success").failureUrl("/failure")
+////                .userInfoEndpoint()
+////                .userService(userService)
+////                .and()
+////               .clientRegistrationRepository(clientRegistrationRepository())
+////                .authorizedClientService(authorizedClientService())
+//                    .authorizationEndpoint()
+//                        .baseUri("/search")
+//                        .authorizationRequestRepository(authorizationRequestRepository())
+//                        .and()
+//                    .redirectionEndpoint()
+//                        .baseUri("/login/oauth/callback/*")
+//                        .and()
+                    .tokenEndpoint()
+                        .accessTokenResponseClient(accessTokenResponseClient());
         return http.build();
     }
 
-    /**
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest>
+    authorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
     }
-    **/
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
+    accessTokenResponseClient() {
+        return new DefaultAuthorizationCodeTokenResponseClient();
+    }
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("client-id")
+                .clientSecret("client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("email")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .clientName("Google")
+                .build();
+    }
+
 }
