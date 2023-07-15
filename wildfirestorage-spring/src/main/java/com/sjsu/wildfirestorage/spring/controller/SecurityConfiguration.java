5cad08b38695e6da7124e5c8957e0d4a1461500e
ben reed <ben.reed@sjsu.edu>
2023-07-15 15:15:38 -0700

    disable CSRF

--- wildfirestorage-spring/src/main/java/com/sjsu/wildfirestorage/spring/controller/SecurityConfiguration.java
+++ wildfirestorage-spring/src/main/java/com/sjsu/wildfirestorage/spring/controller/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +13,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(ac -> ac.requestMatchers("/api/**").permitAll().anyRequest().authenticated())
+        http.csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(ac -> ac.requestMatchers("/api/**").permitAll().anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults()).logout(Customizer.withDefaults());
         return http.build();
     }
