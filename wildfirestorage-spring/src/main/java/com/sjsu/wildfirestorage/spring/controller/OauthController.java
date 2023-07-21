aa007dae0fd5515a25580b952294545587d15581
peeoke <118573213+peeoke@users.noreply.github.com>
2023-07-20 17:03:01 -0700

    oauth support (#22)

---
+++ wildfirestorage-spring/src/main/java/com/sjsu/wildfirestorage/spring/controller/OauthController.java
@@ -0,0 +1,15 @@
+package com.sjsu.wildfirestorage.spring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OauthController {
+    @GetMapping("/")
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<>("hello", HttpStatus.OK);
+    }
+}
+
