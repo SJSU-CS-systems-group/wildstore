4c556c4adddba93f726837c84ae76f580d657796
ben reed <ben.reed@sjsu.edu>
2024-02-15 22:02:51 -0800

    allowed share command to take a list of files to share
    also includes code reformatting cleanup

--- wildfirestorage-cli/src/main/java/com/sjsu/wildfirestorage/Main.java
+++ wildfirestorage-cli/src/main/java/com/sjsu/wildfirestorage/Main.java
@@ -1,7 +1,9 @@
 package com.sjsu.wildfirestorage;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
 import picocli.CommandLine;
 
 import java.nio.file.Files;
@@ -23,24 +25,35 @@ public class Main {
         public void datasetInfo(@CommandLine.Parameters(paramLabel = "fileName") String fileName, @CommandLine.Parameters(paramLabel = "hostname") String hostname) throws InterruptedException {
             System.out.println("GET returned: " + Client.get(Client.getWebClient(hostname + "/api/metadata"),
                     new LinkedMultiValueMap<String, String>(Map.of("filename", List.of(fileName))),
-                    new ParameterizedTypeReference<ArrayList<Metadata>>(){}));
+                    new ParameterizedTypeReference<ArrayList<Metadata>>() {
+                    }));
         }
 
         @CommandLine.Command
-        public void share(@CommandLine.Option(description = "Absolute file name", names = "--file", required = true) String fileName,
-                          @CommandLine.Parameters(paramLabel = "hostname") String hostname,
-                          @CommandLine.Option(names = "--token") String token) throws InterruptedException, ExecutionException {
-            System.out.println("POST returned: " + Client.post(Client.getWebClient(hostname + "/api/share-link/create"),
-                    fileName.replace("\"", ""),
-                    new ParameterizedTypeReference<String>(){}, httpHeaders -> {
-                        httpHeaders.setBearerAuth(token);
-                    }));
+        public void share(@CommandLine.Option(names = "--meta-url", description = "URL of metadata server", defaultValue = "http://127.0.0.1:27777") String metaURL,
+                          @CommandLine.Parameters(description = "Absolute file name", index = "0..*") String[] fileNames,
+                          @CommandLine.Option(names = "--token", required = true) String token) throws InterruptedException, ExecutionException {
+            for (var fileName : fileNames) {
+                try {
+                    System.out.println(Client.post(Client.getWebClient(metaURL + "/api/share-link/create"),
+                            fileName,
+                            new ParameterizedTypeReference<String>() {
+                            }, httpHeaders -> {
+                                httpHeaders.setBearerAuth(token);
+                            }));
+                } catch (ExecutionException e) {
+                    var message = e.getMessage();
+                    // if this is a message about a connection problem, drop all the text before connection
+                    if (message.contains("Connection")) message = message.substring(message.indexOf("Connection"));
+                    System.err.printf("%s: %s\n", message, fileName);
+                }
+            }
         }
 
         @CommandLine.Command
         public void search(@CommandLine.Parameters(paramLabel = "query") String query, @CommandLine.Parameters(paramLabel = "hostname") String hostname,
                            @CommandLine.Parameters(paramLabel = "<option>", defaultValue = "all", description = "Which information to print - 'all' or 'basic'") String option,
-                           @CommandLine.Option(names="--limit", defaultValue = "10") int limit,
+                           @CommandLine.Option(names = "--limit", defaultValue = "10") int limit,
                            @CommandLine.Option(names = "--offset", defaultValue = "0") int offset,
                            @CommandLine.Option(names = "--token") String token) throws InterruptedException, ExecutionException {
 
@@ -48,20 +61,20 @@ public class Main {
             metadataRequest.searchQuery = query;
             metadataRequest.limit = limit;
             metadataRequest.offset = offset;
-            metadataRequest.excludeFields = new String[] {"globalAttributes", "variables"};
+            metadataRequest.excludeFields = new String[]{"globalAttributes", "variables"};
             WebClient webClient = Client.getWebClient(hostname + "/api/metadata/search?excludeFields=globalAttributes,variables");
-            var res = (ArrayList<Metadata>)(Client.post(webClient, metadataRequest, new ParameterizedTypeReference<ArrayList<Metadata>>(){}, httpHeaders -> {
+            var res = (ArrayList<Metadata>) (Client.post(webClient, metadataRequest, new ParameterizedTypeReference<ArrayList<Metadata>>() {
+            }, httpHeaders -> {
                 httpHeaders.setBearerAuth(token);
             }));
             System.out.println("SEARCH returned: " + res.size() + " results");
             if (option.equals("all")) {
-                for (Metadata m: res) {
+                for (Metadata m : res) {
                     System.out.println("========================================================================");
                     PrintData.printAllData(m);
                 }
-            }
-            else {
-                for (Metadata m: res){
+            } else {
+                for (Metadata m : res) {
                     System.out.println("========================================================================");
                     PrintData.printBasic(m);
                 }
@@ -77,18 +90,20 @@ public class Main {
             LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
             parameters.add("limit", String.valueOf(limit));
             List<String> result;
-            int i=0;
+            int i = 0;
             WebClient webClient = Client.getWebClient(hostname + "/api/metadata/filepath");
 
             do {
                 parameters.put("offset", List.of(String.valueOf(offset)));
                 result = (List<String>) Client.get(webClient,
                         parameters,
-                        new ParameterizedTypeReference<List<String>>() {});
+                        new ParameterizedTypeReference<List<String>>() {
+                        });
                 System.out.println("The following Metadata documents will be removed from the database:");
                 result.forEach(str -> System.out.println(str));
                 List<String> deletedFiles = result.stream().filter(item -> !Files.exists(Paths.get(item))).toList();
-                System.out.println("DELETE RESULT:" + Client.post(webClient, deletedFiles, new ParameterizedTypeReference<Integer>() {}, httpHeaders -> {
+                System.out.println("DELETE RESULT:" + Client.post(webClient, deletedFiles, new ParameterizedTypeReference<Integer>() {
+                }, httpHeaders -> {
                     httpHeaders.setBearerAuth(token);
                 }));
                 offset += limit;
