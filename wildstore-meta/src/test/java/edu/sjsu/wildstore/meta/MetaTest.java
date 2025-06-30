79f5dac0bdefc23eb8a66557ca1111069eaa0522
alder <137121016+alderwang06@users.noreply.github.com>
2025-06-30 16:53:59 -0700

    Backup restore (#78)
    * auto backup should work and can be specified in xml file
    
    * cleanup
    
    * cleanup
    
    * cleanup

--- wildstore-meta/src/test/java/edu/sjsu/wildstore/meta/MetaTest.java
+++ wildstore-meta/src/test/java/edu/sjsu/wildstore/meta/MetaTest.java
@@ -5,18 +5,24 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
 
 @SpringBootTest(classes = {TestOAuthSecurityConfig.class, Main.class})
 @TestPropertySource(locations = {"classpath:application-test.properties"})
+@EnableScheduling
 public class MetaTest {
 
     @Autowired
@@ -27,6 +33,9 @@ public class MetaTest {
 
     static MongoTemplate staticMongoTemplate;
 
+    @TempDir
+    static Path backupDir;
+
     @DynamicPropertySource
     static void setProps(DynamicPropertyRegistry registry) throws IOException {
         var sock = new ServerSocket(0);
@@ -36,6 +45,8 @@ public class MetaTest {
         registry.add("spring.data.mongodb.uri", () -> "mongodb://localhost:27017/wildstore-test-" + System.currentTimeMillis());
         registry.add("server.port", () -> port);
         registry.add("wildstore.initialAdmins", () -> "test@example.com");
+        registry.add("wildstore.backupDirectory", () -> backupDir.toString());
+        registry.add("backup.interval", () -> "500"); // .5 second for testing
     }
 
     @BeforeEach
@@ -57,5 +68,15 @@ public class MetaTest {
         var userList = userService.getUserList();
         Assertions.assertEquals(1, userList.size());
         Assertions.assertEquals("ROLE_ADMIN", userList.get(0).get("role"));
+    }
+
+    @Test
+    public void testBackup() throws IOException, InterruptedException {
+        FileTime firstTime = Files.getLastModifiedTime(backupDir.resolve("share-links.json"));
+        Assertions.assertTrue(Files.exists(backupDir.resolve("share-links.json")));
+        Assertions.assertTrue(Files.exists(backupDir.resolve("userData.json")));
+
+        Thread.sleep(1000);
+        Assertions.assertTrue(firstTime.compareTo(Files.getLastModifiedTime(backupDir.resolve("share-links.json"))) < 0);
     }
 }
