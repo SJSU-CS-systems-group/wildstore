9bce5673841ea742b3d3412395c2f13f7ae6845e
Adelyn Tam <52673806+adelyntam@users.noreply.github.com>
2025-11-26 14:17:17 -0800

    search var dropdown overflow
    can see the dropdown menu as it overflows into the search results.
    tooltip still has some issues (not urgent) that it does not overflow out of the dropdown menu.

--- wildstore-meta/app/src/components/autocomplete/autocomplete.jsx
+++ wildstore-meta/app/src/components/autocomplete/autocomplete.jsx
@@ -7,72 +7,80 @@ const Autocomplete = ({ items, value, onChange }) => {
   const [filteredItems, setFilteredItems] = useState([...items]);
 
   const filter = (e) => {
-    let varName = e.target.value;
+    const varName = e.target.value;
     if (varName) {
       const newItems = items
         .filter((p) => p.label.toLowerCase().includes(varName.toLowerCase()))
-        .sort((p1, p2) => (p1.label.localeCompare(p2.label)));
+        .sort((p1, p2) => p1.label.localeCompare(p2.label));
       setFilteredItems(newItems);
     } else {
       setFilteredItems([...items]);
     }
-  }
+  };
 
   return (
     <div className="join-item">
       <div
-        // use classnames here to easily toggle dropdown open 
-        className={classNames({
-          "dropdown w-full": true,
-          "dropdown-open": open
-        })}
         ref={ref}
+        className={classNames("dropdown w-full relative z-30", { "dropdown-open": open })}
       >
         <input
           type="text"
-          className="input input-bordered join-item focus:outline outline-offset-2 outline-2 outline-gray-300"
+          className="w-full input input-bordered join-item focus:outline outline-offset-2 outline-2 outline-gray-300"
           value={value}
           onChange={(e) => {
-            onChange(e)
-            filter(e)
+            onChange(e);
+            filter(e);
+            setOpen(true);
           }}
           onFocus={(e) => {
-            onChange(e)
-            filter(e)
+            onChange(e);
+            filter(e);
+            setOpen(true);
           }}
+          onBlur={() => setOpen(false)}
           placeholder="variable"
           tabIndex={0}
-          style={{ "width": "inherit" }}
+          style={{ width: "inherit" }}
         />
-        <div className="dropdown-content border border-base-200 top-14 overflow-scroll overflow-y-scroll h-40 flex-col rounded-md"
-          style={{ "position": "absolute", "zIndex": "1", "backgroundColor": "white", "marginTop": "-8px", "width": "250%" }}>
+
+        <div
+          className="dropdown-content border border-base-200 top-14 overflow-y-scroll h-40 flex-col rounded-md absolute z-50 w-max min-w-full"
+          style={{
+            backgroundColor: "white",
+            marginTop: "-8px",
+            width: "250%",
+          }}
+        >
           <ul
-            className="menu menu-compact  last:border-b-0"
-            // use ref to calculate the width of parent
-            style={{ width: ref.current?.clientWidth }}
+            className="w-full menu menu-compact last:border-b-0"
           >
-            {filteredItems.map((item, index) => {
-              return (
-                <li
-                  key={index}
-                  tabIndex={index + 1}
-                  onClick={(e) => {
-                    setOpen(false);
-                    onChange(e);
-                    filter(e)
-                  }}
-                  className="border-b border-b-base-content/10 w-full"
+            {filteredItems.map((item, index) => (
+              <li
+                key={index}
+                tabIndex={index + 1}
+                onClick={(e) => {
+                  setOpen(false);
+                  onChange(e);
+                  filter(e);
+                }}
+                className="border-b border-b-base-content/10 w-full"
+              >
+                <button
+                  className="before:z-5000 before:content-[attr(data-tip)] tooltip tooltip-right"
+                  data-tip={item.value}
+                  value={item.label}
                 >
-                  <button className="before:z-5000 before:content-[attr(data-tip)] tooltip tooltip-right" data-tip={item.value}
-                    value={item.label}>{item.label}</button>
-                </li>
-              );
-            })}
+                  {item.label}
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default memo(Autocomplete);
