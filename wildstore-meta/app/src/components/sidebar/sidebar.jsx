1d932a4f2c2a477a68a07342c3e123fadaf1891e
Ahmad Gazali <105146133+ahmadgaz@users.noreply.github.com>
2025-11-27 20:16:02 -0800

    remove unnecessary sidebar icons (#111)

--- wildstore-meta/app/src/components/sidebar/sidebar.jsx
+++ wildstore-meta/app/src/components/sidebar/sidebar.jsx
@@ -1,4 +1,4 @@
-import { GoHome, GoLocation, GoSearch, GoShareAndroid, GoPerson } from 'react-icons/go';
+import { GoHome, GoShareAndroid, GoPerson } from 'react-icons/go';
 import SideBarIcon from '../sidebar-icon/sidebarIcon';
 import { Link } from "react-router-dom";
 const SideBar = () => {
@@ -7,8 +7,6 @@ const SideBar = () => {
             <Link to="/home">
                 <SideBarIcon icon={<GoHome size="28" />} />
             </Link>
-            <SideBarIcon icon={<GoLocation size="28" />} />
-            <SideBarIcon icon={<GoSearch size="28" />} />
             <Link to="/home/share">
                 <SideBarIcon icon={<GoShareAndroid size="28" />} />
             </Link>
