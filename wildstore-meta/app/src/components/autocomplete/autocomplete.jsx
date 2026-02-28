204e015addc9c97cc822b8011f34510388a2b182
ahmadgaz <gazali.contact@gmail.com>
2026-02-28 13:45:52 -0800

    convert inline styles to Tailwind utility classes

--- wildstore-meta/app/src/components/autocomplete/autocomplete.jsx
+++ wildstore-meta/app/src/components/autocomplete/autocomplete.jsx
@@ -41,16 +41,10 @@ const Autocomplete = ({ items, value, onChange }) => {
           onBlur={() => setOpen(false)}
           placeholder="variable"
           tabIndex={0}
-          style={{ width: "inherit" }}
         />
 
         <div
-          className="dropdown-content border border-base-200 top-14 overflow-y-scroll h-40 flex-col rounded-md absolute z-50 w-max min-w-full"
-          style={{
-            backgroundColor: "white",
-            marginTop: "-8px",
-            width: "250%",
-          }}
+          className="dropdown-content border border-base-200 top-14 overflow-y-scroll h-40 flex-col rounded-md absolute z-50 bg-white -mt-2 !w-[250%]"
         >
           <ul
             className="w-full menu menu-compact last:border-b-0"
