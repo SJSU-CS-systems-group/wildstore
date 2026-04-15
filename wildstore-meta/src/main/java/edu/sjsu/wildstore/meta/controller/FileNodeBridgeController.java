048339726da03ff9298e4dab41b9d9e15998ad9f
Adelyn Tam <52673806+adelyntam@users.noreply.github.com>
2026-04-14 19:11:21 -0700

    Update FileNodeBridgeController.java

--- wildstore-meta/src/main/java/edu/sjsu/wildstore/meta/controller/FileNodeBridgeController.java
+++ wildstore-meta/src/main/java/edu/sjsu/wildstore/meta/controller/FileNodeBridgeController.java
@@ -9,16 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import edu.sjsu.wildstore.wildstore_relationalDb.resources.FileNode;
+
 @RestController
 public class FileNodeBridgeController {
     @PreAuthorize("hasRole('GUEST')")
     @GetMapping("/file_contents")
-    public List<?> files(@RequestParam(value = "file_id", defaultValue = "0") String fileId) {
+    public List<FileNode> files(@RequestParam(value = "file_id", defaultValue = "0") String fileId) {
         try {
             Class<?> controllerClass = Class.forName("edu.sjsu.wildstore.wildstore_relationalDb.controller.FileNodeController");
             Object fileNodeController = controllerClass.getDeclaredConstructor().newInstance();
             Object response = controllerClass.getMethod("files", String.class).invoke(fileNodeController, fileId);
-            return (List<?>) response;
+            return (List<FileNode>) response;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
                     "Unable to load FileNodeController data", e);
