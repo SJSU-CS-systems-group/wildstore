247659fefc0de67ac0eb799318a66b2384f7fcd6
Chase Porter <chaseporter15@gmail.com>
2026-06-01 14:59:10 -0700

    Serves FileNodes from relational-db

--- wildstore-meta/src/main/java/edu/sjsu/wildstore/meta/controller/FileNodeBridgeController.java
+++ wildstore-meta/src/main/java/edu/sjsu/wildstore/meta/controller/FileNodeBridgeController.java
@@ -2,6 +2,13 @@ package edu.sjsu.wildstore.meta.controller;
 
 import java.util.List;
 
+import java.util.Arrays;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +20,25 @@ import edu.sjsu.wildstore.wildstore_relationalDb.records.FileNodeRecord;
 
 @RestController
 public class FileNodeBridgeController {
+
+    @Value("${custom.sqlServerUrl}")
+    private String sqlServerUrl;
+
+    private final RestClient restClient = RestClient.create();
+
     @PreAuthorize("hasRole('GUEST')")
     @GetMapping("/file_contents")
-    public List<FileNodeRecord> files(@RequestParam(value = "file_id", defaultValue = "0") String fileId) {
+    public ResponseEntity<List<FileNodeRecord>> getFileNodes(@RequestParam(value = "file_id", defaultValue = "0") String fileId) {
         try {
-            Class<?> controllerClass = Class.forName("edu.sjsu.wildstore.wildstore_relationalDb.controller.FileNodeController");
-            Object fileNodeController = controllerClass.getDeclaredConstructor().newInstance();
-            Object response = controllerClass.getMethod("files", String.class).invoke(fileNodeController, fileId);
-            return (List<FileNodeRecord>) response;
+            String url = UriComponentsBuilder.fromHttpUrl(sqlServerUrl)
+                    .path("/file_contents")
+                    .queryParam("file_id", fileId)
+                    .toUriString();
+
+            FileNodeRecord[] response = restClient.get().uri(url).retrieve().body(FileNodeRecord[].class);
+            List<FileNodeRecord> responseList = Arrays.asList(response);
+
+            return ResponseEntity.ok(responseList);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
                     "Unable to load FileNodeController data", e);
